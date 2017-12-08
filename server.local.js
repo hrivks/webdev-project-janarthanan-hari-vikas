1639cec70490917bff2535be244a6e7e6fffa6df
hrivks <hrivks@gmail.com>
2017-12-08 18:49:31 -0500

    local env variables

--- server.local.js
+++ server.local.js
@@ -1,14 +1,19 @@
 /** DO NOT COMMIT THIS FILE : has sensitive api keys
 * environment variables for local development 
+* ------------------------------------------------------------------------------------------
+* Change this file locally and use the following command to stop git from tracking changes
+* git update-index --assume-unchanged server.local.js
+* ------------------------------------------------------------------------------------------
 * */
+
 // cookie session enc key
 process.env.SESSION_SECRET = 'ABCDED';
 // mongo db connection string
 process.env.MONGODB_URI = 'mongodb://localhost:27017/writeme';
 // Icon finder app info
-process.env.ICONFINDER_API_CLIENT_ID = 'ssckMfWKPCd1IddzuZ8SOXIZHuRgHehsy1c1HecmYh1ePP8QD5pUepP1UPIwSLPR';
-process.env.ICONFINDER_API_CLIENT_SECRET = 'jkdNot6rqIZmKPXtujS83qxq7hVGYngkhLzuD02qA9oLt5cgcIwSEA0RFPL1dLjy';
+process.env.ICONFINDER_API_CLIENT_ID = '';
+process.env.ICONFINDER_API_CLIENT_SECRET = '';
 // Github app info
-process.env.GITHUB_CLIENT_ID = '56b2448af249740c1910';
-process.env.GITHUB_CLIENT_SECRET = '063cd7fb7dabf9dd9ec252f5ae743e9f64896442';
+process.env.GITHUB_CLIENT_ID = '';
+process.env.GITHUB_CLIENT_SECRET = '';
 process.env.GITHUB_CALLBACK_URL = 'http://localhost:3100/api/auth/github/callback';
